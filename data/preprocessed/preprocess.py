1b1848976fda382fca7770a0316e7a4840810dd2
Khushi Madan <khushimadan19@gmail.com>
2026-03-01 19:04:16 +0530

    Update preprocess.py

--- data/preprocessed/preprocess.py
+++ data/preprocessed/preprocess.py
@@ -5,8 +5,8 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
-# nltk.download("stopwords")
-# nltk.download("wordnet")
+nltk.download("stopwords")
+nltk.download("wordnet")
 
 stop_words = set(stopwords.words("english"))
 negation_words = {"not", "nor", "never", "no"}
